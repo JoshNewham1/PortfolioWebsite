973af991c2aba9235625ab0aed726aa16a0c53eb
Josh Newham <joshnewham@live.com>
2022-10-19 12:28:47 +0100

    Added info popup

--- components/Skill.js
+++ components/Skill.js
@@ -1,4 +1,7 @@
 import SkillLevel from "./SkillLevel";
+import { usePopperTooltip } from "react-popper-tooltip";
+import infoIcon from "../public/info.png";
+import Image from "next/image";
 
 const skillLevelDescriptions = {
   1: "Still learning",
@@ -9,12 +12,33 @@ const skillLevelDescriptions = {
 };
 
 export default function Skill({ skill }) {
+  const { getTooltipProps, setTooltipRef, setTriggerRef, visible } =
+    usePopperTooltip({ placement: "top" });
   return (
     <div className="p-6 rounded-lg shadow-lg bg-white dark:bg-gray-700 mr-1 ml-1 mb-2 md:mr-4 md:ml-4 md:mb-4">
-      <h2 className="mb-2">{skill.name}</h2>
+      <div className="flex flex-row">
+        {/* Title and info button */}
+        <h2 className="mb-2 mr-2">{skill.name}</h2>{" "}
+        <button ref={setTriggerRef}>
+          <Image fill="white" src={infoIcon} height={24} width={24} />
+        </button>
+      </div>
+
       <SkillLevel level={skill.skillLevel} />
       <h3>{skill.skillLevel} / 5</h3>
       <p className="text-sm">{skillLevelDescriptions[skill.skillLevel]}</p>
+
+      {visible && (
+        <div
+          ref={setTooltipRef}
+          {...getTooltipProps({
+            className:
+              "tooltip-container p-4 rounded-lg shadow-lg bg-white dark:bg-gray-700 md:w-96",
+          })}
+        >
+          {skill.description}
+        </div>
+      )}
     </div>
   );
 }
